22cf812e99bc6e9b6044f55fb5399e6f5fa6d09a
surajmanjhi01 <surajmanjhi303@gmail.com>
2025-12-07 14:15:17 +0000

    variable learning

---
+++ 01_Basics/01_Variable.js
@@ -0,0 +1,14 @@
+const accountId=14453
+let accountEmail="Mannjhi@google.com"
+var accountPassword="123"
+accountCity="Jaipur"
+let accountState;
+accountEmail="Havour.com"
+accountPassword="456"
+accountCity="Delhi"
+
+/*
+Prefer not to use var
+beacuse of issue in functional scope r bloack scope
+*/
+console.table([accountEmail,accountPassword,accountCity,accountState])
