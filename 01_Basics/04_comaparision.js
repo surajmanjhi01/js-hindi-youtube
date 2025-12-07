49a35533ab2a78a4c8931fae7019bb7a93a89b25
surajmanjhi01 <surajmanjhi303@gmail.com>
2025-12-07 19:17:47 +0000

    concept of stack and heap meemory

---
+++ 01_Basics/04_comaparision.js
@@ -0,0 +1,23 @@
+// console.log(2>1);
+
+//*** */
+//Stack(Primitive),Heap(Non-Primitive)
+
+let myYtName="ManjhiVlogs"
+
+let anothername=myYtName
+anothername="surajblogs"
+
+let userOne ={
+   email:"user@google.com",
+   upi:"user@byt"
+
+}
+let userTwo=userOne
+userTwo.email="hits@google.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
+
+// console.log(anothername);
+// console.log(myYtName);
--- 01_Basics/conversion_Operation.js
+++ 01_Basics/conversion_Operation.js
@@ -1,9 +1,27 @@
 let score="33abc"
-console.log(typeof(score));
-console.log(typeof(score));
+// console.log(typeof(score));
+// console.log(typeof(score));
 
-let valueInNumber=Number(score);
-console.log(typeof valueInNumber);
+ let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
 
 //"33"=>>Number
-//
+
+
+//**********Operations***********//
+let value=3
+let negvalue=-value
+// console.log(negvalue);
+
+let str1="Hello"
+let str2=" Manjhi"
+let str3=str1+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2")
+console.log(true);
+console.log(+true);
+// console.log(true+);
