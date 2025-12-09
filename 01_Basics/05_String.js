54882c5720eb71c2bd798f6f57b65dac3fdd44ab
surajmanjhi01 <surajmanjhi303@gmail.com>
2025-12-09 07:59:27 +0000

    mathrandom

---
+++ 01_Basics/05_String.js
@@ -0,0 +1,9 @@
+const Name="Manjhi"
+const repoCount=80
+
+//console.log(name+repoCount+"Value")   This is Old skool method
+//new method using backTick
+
+console.log(`Hello my name is ${Name} and my repocount is ${repoCount}`);
+
+
---
+++ 01_Basics/06_Numsandmaths.js
@@ -0,0 +1,16 @@
+// const score = 400
+// console.log(score);
+
+// const Balance= new Number(100);
+// console.log(Balance)
+
+// console.log(Balance.toString());
+console.log(Math.random());
+console.log(Math.random()*10+1);
+console.log((Math.random()*10)+1);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
