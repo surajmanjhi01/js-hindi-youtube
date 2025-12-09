1e4de39c8000990365bf518b94f46f349e7ce7eb
surajmanjhi01 <surajmanjhi303@gmail.com>
2025-12-09 18:00:15 +0000

    Objects imp

---
+++ 02_Basics/03_Objects.js
@@ -0,0 +1,14 @@
+//singleton
+
+const mySym= Symbol=
+//object literals
+const Jsuser={
+   name:"MANJHI",
+   age:18,
+   location:"Delhi",
+   email:"LALA@locationbar.com",
+   isLoggedin:false,
+   LastLoginDays: ["Monday","Sunday"]
+} 
+console.log(Jsuser.email)
+console.log(Jsuser["email"])
---
+++ 02_Basics/Array.js
@@ -0,0 +1,13 @@
+//array
+
+const myArr=[0,1,2,3,4]
+
+console.log("A",myArr)
+
+const myn1=myArr.slice(1,3)
+console.log(myn1)
+console.log("B",myArr)
+
+const myn2=myArr.splice(1,3)
+console.log("C",myArr)
+console.log(myn2)
